9d545efb7730f8bb1db7968ab0504982553b2cbf
Alireza-shahbaziyan <alishahbazi0831@gmail.com>
2022-04-12 22:27:19 +0430

    frist commit

---
+++ app.js
@@ -0,0 +1,125 @@
+const canvas = document.querySelector('canvas');    // choice canvas tag
+
+canvas.width = window.innerWidth - 10;
+canvas.height = window.innerHeight - 10;
+const c = canvas.getContext('2d') // c means Canvas
+
+const count = 5;
+
+class Ball {
+    constructor() {
+        this.r = 40;
+        this.x = random(0 + this.r, window.innerWidth);
+        this.y = random(0 + this.r, window.innerHeight);
+        this.vx = random(0,7);
+        this.vy = random(0,7);
+        this.draw()
+    }
+    draw() {
+        c.beginPath()
+        c.arc(this.x, this.y, this.r, 0, 2 * Math.PI)
+        c.fillStyle = 'white'
+        c.fill()
+    }
+    update() {
+        if (this.x + this.r > window.innerWidth || this.x - this.r < 0) {
+            this.vx = -this.vx
+        }
+        if (this.y + this.r > window.innerHeight || this.y - this.r < 0) {
+            this.vy = -this.vy
+        }
+        this.x +=this.vx;
+        this.y += this.vy;
+        this.draw()
+    }
+}
+
+let balls = []
+
+for (let i = 0; i < count; i++) {
+    balls.push(new Ball)
+}
+
+function animation() {
+    c.clearRect(0, 0, window.innerWidth, window.innerHeight);
+    balls.forEach(ball => {
+        ball.update();
+    });
+    requestAnimationFrame(animation);
+}
+
+animation()
+
+
+
+// Returns an integer random number between min (included) and max (included)
+function random(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let r = 40;
+// let x = random(2 + r, window.innerWidth);
+// let y = random(0 + r, window.innerHeight);
+// let vx = Math.random() * 3;
+// let vy = Math.random() * 3;
+
+// function animation() {
+
+//     c.beginPath()
+//     c.arc(x, y, r, 0, 2 * Math.PI)
+//     c.fillStyle = 'white'
+//     c.fill()
+//     if (x + r > window.innerWidth || x - r < 0) {
+//         vx = -vx
+//     }
+//     if (y + r > window.innerHeight || y - r < 0) {
+//         vy = -vy
+//     }
+//     x += vx;
+//     y += vy;
+//     //------------------------------------------
+//     requestAnimationFrame(animation)
+// }
+
+
+
+// animation()
